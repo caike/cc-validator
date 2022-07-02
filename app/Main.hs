52807a32e712783d9761f04d67eadca9d875a7cb
Carlos Souza <caike@users.noreply.github.com>
2022-07-02 16:56:02 -0400

    Expose HTTP endpoint (#5)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Lib
+import Lib (isValidCardStr)
+
+import qualified Data.Text.Lazy as T
+import Web.Scotty
 
 main :: IO ()
-main = print "Hello"
+main = scotty 3000 $
+  get "/" $ do
+    number <- T.unpack <$> param "number" `rescue` (\_ -> return "")
+    let message = validate number
+    html message
+
+validate :: String -> T.Text
+validate "" = "Invalid"
+validate number =
+    let isValid = if isValidCardStr number then "Valid" else "Invalid"
+        in T.pack isValid
